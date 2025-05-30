a2c37e881d752a4195fa82c2e9da16dad49ab7a5
Shahvalat Mirzayev <shahvalatmirzayev@gmail.com>
2025-05-30 23:06:46 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Smooth scroll for all anchor links with #
+  
   document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener("click", function (e) {
       e.preventDefault();
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
-  // Smooth scroll to top for logo
+  
   const logo = document.querySelector('.logo');
   if (logo) {
     logo.addEventListener("click", function (e) {
